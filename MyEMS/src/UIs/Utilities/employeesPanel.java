cf47d7a08f793a42aa4e8cb00883e3a41eb09af0
Matthew Akrigg <138000221+matthewakrigg05@users.noreply.github.com>
2024-06-07 09:48:06 +0100

    feat: displaying employee info

--- MyEMS/src/UIs/Utilities/employeesPanel.java
+++ MyEMS/src/UIs/Utilities/employeesPanel.java
@@ -1,13 +1,33 @@
 package UIs.Utilities;
+import DB_Usage.Employee;
 import DB_Usage.User;
 import UIs.myEmsUI;
 
 import javax.swing.*;
 
+import java.util.ArrayList;
+
+import static DB_Usage.employeesDB.getEmployees;
+
 public class employeesPanel extends JDialog {
 
-    public static JPanel getEmployeePanel(User user, myEmsUI myEmsUi){
+    public static JScrollPane getEmployeePanel(User user, myEmsUI myEmsUi){
         final JPanel employeesPanel = new JPanel();
+        JScrollPane scrollPane = new JScrollPane(employeesPanel);
+        employeesPanel.setAutoscrolls(true);
+
+        ArrayList<Employee> employees = getEmployees(user);
+        int numOfEmployees = employees.size();
+
+        for(int i = 0; i < numOfEmployees; i++){
+            String fname = employees.get(i).getFname();
+
+
+
+            JTextArea employeeInfo = new JTextArea(fname);
+            employeesPanel.add(employeeInfo);
+        }
+
         JLabel employeeTitle = new JLabel("Employees");
 
         JLabel addEmployeeLabel = new JLabel("Add Employee");
@@ -25,6 +45,6 @@ public class employeesPanel extends JDialog {
             addEmployeeWindow.setVisible(true);
         });
 
-        return employeesPanel;
+        return scrollPane;
     }
 }
