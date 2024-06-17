b3b0cb9851bb0315590133875031869572e2e539
Matthew Akrigg <138000221+matthewakrigg05@users.noreply.github.com>
2024-06-17 14:21:35 +0100

    refactor: made sidebar utility

--- MyEMS/src/UIs/myEmsUI.java
+++ MyEMS/src/UIs/myEmsUI.java
@@ -23,68 +23,7 @@ public class myEmsUI extends mainFrame implements ActionListener {
 
     @Override
     protected void addUIcomponents() {
-        // Components associated to the sidebar:
-        final JToolBar sideBar = new JToolBar(JToolBar.VERTICAL);
-        JLabel placeholder = new JLabel("MyEMS");
-        JButton homeButton = new JButton("Home");
-        JButton employeesButton = new JButton("Employees");
-        JButton rotasButton = new JButton("Rotas");
-        JButton calendarButton = new JButton("Calendar");
-        JButton settingsButton = new JButton("Settings");
-
-        sideBar.setBackground(Color.blue);
-        sideBar.setPreferredSize(new Dimension(240, 500));
-        sideBar.setFloatable(false);
-        sideBar.add(placeholder);
-        sideBar.add(homeButton);
-        sideBar.add(employeesButton);
-        sideBar.add(rotasButton);
-        sideBar.add(calendarButton);
-        sideBar.add(settingsButton);
-
-        // Action Listeners for the sidebar buttons, removes all panels then replaces them with the panels they
-        // intend to see
-        homeButton.addActionListener(e -> {
-            getContentPane().removeAll();
-            getContentPane().add(sideBar, BorderLayout.WEST);
-            getContentPane().add(homePanel.getHomePanel(), BorderLayout.CENTER);
-            revalidate();
-            repaint();
-        });
-
-        employeesButton.addActionListener(e -> {
-            getContentPane().removeAll();
-            getContentPane().add(sideBar, BorderLayout.WEST);
-            getContentPane().add(employeesPanel.getEmployeePanel(user, this), BorderLayout.CENTER);
-            revalidate();
-            repaint();
-        });
-
-        rotasButton.addActionListener(e -> {
-            getContentPane().removeAll();
-            getContentPane().add(sideBar, BorderLayout.WEST);
-            getContentPane().add(rotasPanel.getRotasPanel(), BorderLayout.CENTER);
-            revalidate();
-            repaint();
-        });
-
-        calendarButton.addActionListener(e -> {
-            getContentPane().removeAll();
-            getContentPane().add(sideBar, BorderLayout.WEST);
-            getContentPane().add(calendarPanel.getCalendarPanel(), BorderLayout.CENTER);
-            revalidate();
-            repaint();
-        });
-
-        settingsButton.addActionListener(e -> {
-            getContentPane().removeAll();
-            getContentPane().add(sideBar, BorderLayout.WEST);
-            getContentPane().add(settingsPanel.getSettingsPanel(this), BorderLayout.CENTER);
-            revalidate();
-            repaint();
-        });
-
         add(homePanel.getHomePanel(), BorderLayout.CENTER);
-        add(sideBar, BorderLayout.WEST);
+        add(sideBar.getSideBar(user, this), BorderLayout.WEST);
     }
 }
