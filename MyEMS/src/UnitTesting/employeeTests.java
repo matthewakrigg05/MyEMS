e5d97c5c76f8189f410a4fa8ba7b1f6ce7d9a251
Matthew Akrigg <matthewakrigg05@gmail.com>
2024-06-20 14:36:44 +0100

    test: employee tests

---
+++ MyEMS/src/UnitTesting/employeeTests.java
@@ -0,0 +1,50 @@
+package UnitTesting;
+import DB_Usage.Employee;
+import org.junit.jupiter.api.Test;
+
+public class employeeTests extends Employee {
+
+    @Test
+    void testGetEmployeeData() {
+    }
+
+    @Test
+    void testGetFname() {
+    }
+
+    @Test
+    void testGetLname() {
+    }
+
+    @Test
+    void testGetEmail() {
+    }
+
+    @Test
+    void testGetPhoneNum() {
+    }
+
+    @Test
+    void testGetAddress() {
+    }
+
+    @Test
+    void testGetNInumber() {
+    }
+
+    @Test
+    void testGetWage() {
+    }
+
+    @Test
+    void testGetHoursWorked() {
+    }
+
+    @Test
+    void testGetEmployeeId() {
+    }
+
+    @Test
+    void testGetJoinDate() {
+    }
+}
